5df70807bb9d6fc6874305d940f0a12234b9cdd0
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-20 23:23:41 +0100

    [#6] Add the deletion confirmation component

---
+++ src/Blazor_PerretTremblay/Modals/DeleteConfirmation.razor.cs
@@ -0,0 +1,38 @@
+﻿using Blazor_PerretTremblay.Models;
+using Blazor_PerretTremblay.Services;
+using Blazored.Modal.Services;
+using Blazored.Modal;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor_PerretTremblay.Modals
+{
+    public partial class DeleteConfirmation
+    {
+        [CascadingParameter]
+        public BlazoredModalInstance ModalInstance { get; set; }
+
+        [Inject]
+        public IDataService DataService { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        private Item item = new Item();
+
+        protected override async Task OnInitializedAsync()
+        {
+            // Get the item
+            item = await DataService.GetById(Id);
+        }
+
+        void ConfirmDelete()
+        {
+            ModalInstance.CloseAsync(ModalResult.Ok(true));
+        }
+
+        void Cancel()
+        {
+            ModalInstance.CancelAsync();
+        }
+    }
+}
