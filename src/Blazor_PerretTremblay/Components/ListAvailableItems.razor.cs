096df1196f252be91b52ff7185587bfcc64d5757
LouisPerret <louisperret0311@gmail.com>
2023-02-22 23:41:48 +0100

    Ajout de la liste

---
+++ src/Blazor_PerretTremblay/Components/ListAvailableItems.razor.cs
@@ -0,0 +1,62 @@
+﻿using Blazor_PerretTremblay.Modals;
+using Blazor_PerretTremblay.Models;
+using Blazor_PerretTremblay.Services;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using Blazorise.DataGrid;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor_PerretTremblay.Components
+{
+    public partial class ListAvailableItems
+    {
+        private List<Item> items;
+
+        private int totalItem;
+
+        [Inject]
+        public IDataService DataService { get; set; }
+
+        [Inject]
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [CascadingParameter]
+        public IModalService Modal { get; set; }
+
+        private async Task OnReadData(DataGridReadDataEventArgs<Item> e)
+        {
+            if (e.CancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            if (!e.CancellationToken.IsCancellationRequested)
+            {
+                items = await DataService.List(e.Page, e.PageSize);
+                totalItem = await DataService.Count();
+            }
+        }
+
+        private async void OnDelete(int id)
+        {
+            var parameters = new ModalParameters();
+            parameters.Add(nameof(Item.Id), id);
+
+            var modal = Modal.Show<DeleteConfirmation>("Delete Confirmation", parameters);
+            var result = await modal.Result;
+
+            if (result.Cancelled)
+            {
+                return;
+            }
+
+            await DataService.Delete(id);
+
+            // Reload the page
+            NavigationManager.NavigateTo("list", true);
+        }
+    }
+}
