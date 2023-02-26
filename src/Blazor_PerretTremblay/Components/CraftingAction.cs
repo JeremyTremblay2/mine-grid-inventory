ff2ec4d9e1bb91eda3e80679c80eb2f8bc9baf16
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-26 18:35:21 +0100

    [#3] Add basic components to do the rest of the project

---
+++ src/Blazor_PerretTremblay/Components/CraftingAction.cs
@@ -0,0 +1,11 @@
+﻿using Blazor_PerretTremblay.Models;
+
+namespace Blazor_PerretTremblay.Components
+{
+    public class CraftingAction
+    {
+        public string Action { get; set; }
+        public int Index { get; set; }
+        public Item Item { get; set; }
+    }
+}
---
+++ src/Blazor_PerretTremblay/Components/CraftingRecipe.cs
@@ -0,0 +1,10 @@
+﻿using Blazor_PerretTremblay.Models;
+
+namespace Blazor_PerretTremblay.Components
+{
+    public class CraftingRecipe
+    {
+        public Item Give { get; set; }
+        public List<List<string>> Have { get; set; }
+    }
+}
