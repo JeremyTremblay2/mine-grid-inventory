9a505a6823376cdac2b745fbca85e5c8964c1715
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-26 19:15:19 +0100

    [#3] Refactor namespaces

---
+++ src/Minecraft.Crafting/Components/CraftingRecipe.cs
@@ -0,0 +1,10 @@
+﻿using Minecraft.Crafting.Models;
+
+namespace Minecraft.Crafting.Components
+{
+    public class CraftingRecipe
+    {
+        public Item Give { get; set; }
+        public List<List<string>> Have { get; set; }
+    }
+}
---
+++ src/Minecraft.Crafting/Pages/List.razor.cs
@@ -0,0 +1,63 @@
+﻿using Minecraft.Crafting.Models;
+using Blazored.LocalStorage;
+using Minecraft.Crafting.Services;
+using Blazorise.DataGrid;
+using Microsoft.AspNetCore.Components;
+using Blazored.Modal.Services;
+using Minecraft.Crafting.Modals;
+using Blazored.Modal;
+
+namespace Minecraft.Crafting.Pages
+{
+    public partial class List
+    {
+        private List<Item> items;
+
+        private int totalItem;
+
+        [Inject]
+        public IDataService DataService { get; set; }
+
+        [Inject]
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [CascadingParameter]
+        public IModalService Modal { get; set; }
+
+        private async Task OnReadData(DataGridReadDataEventArgs<Item> e)
+        {
+            if (e.CancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            if (!e.CancellationToken.IsCancellationRequested)
+            {
+                items = await DataService.List(e.Page, e.PageSize);
+                totalItem = await DataService.Count();
+            }
+        }
+
+        private async void OnDelete(int id)
+        {
+            var parameters = new ModalParameters();
+            parameters.Add(nameof(Item.Id), id);
+
+            var modal = Modal.Show<DeleteConfirmation>("Delete Confirmation", parameters);
+            var result = await modal.Result;
+
+            if (result.Cancelled)
+            {
+                return;
+            }
+
+            await DataService.Delete(id);
+
+            // Reload the page
+            NavigationManager.NavigateTo("list", true);
+        }
+    }
+}
