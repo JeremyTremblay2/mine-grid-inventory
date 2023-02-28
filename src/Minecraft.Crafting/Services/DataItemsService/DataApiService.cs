ba003499ec557cdce2a23db581e5efb11e347b65
LouisPerret <louisperret0311@gmail.com>
2023-02-28 21:03:52 +0100

    [#24] Add the last commentary and fix the sln

--- src/Minecraft.Crafting/Services/DataItemsService/DataApiService.cs
+++ src/Minecraft.Crafting/Services/DataItemsService/DataApiService.cs
@@ -4,15 +4,26 @@ using Minecraft.Crafting.Models;
 
 namespace Minecraft.Crafting.Services.DataItemsService
 {
+    /// <summary>
+    /// Service for the loading and the saving of items.
+    /// </summary>
     public class DataApiService : IDataItemsService
     {
+        /// <summary>
+        /// Http client to request the api.
+        /// </summary>
         private readonly HttpClient _http;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="http">Http client to request the api.</param>
         public DataApiService(HttpClient http)
         {
             _http = http;
         }
 
+        /// <inheritdoc/>
         public async Task Add(ItemModel model)
         {
             // Get the item
@@ -22,21 +33,25 @@ namespace Minecraft.Crafting.Services.DataItemsService
             await _http.PostAsJsonAsync("https://localhost:7234/api/Crafting/", item);
         }
 
+        /// <inheritdoc/>
         public async Task<int> Count()
         {
             return await _http.GetFromJsonAsync<int>("https://localhost:7234/api/Crafting/count");
         }
 
+        /// <inheritdoc/>
         public async Task<List<Item>> List(int currentPage, int pageSize)
         {
             return await _http.GetFromJsonAsync<List<Item>>($"https://localhost:7234/api/Crafting/?currentPage={currentPage}&pageSize={pageSize}");
         }
 
+        /// <inheritdoc/>
         public async Task<Item> GetById(int id)
         {
             return await _http.GetFromJsonAsync<Item>($"https://localhost:7234/api/Crafting/{id}");
         }
 
+        /// <inheritdoc/>
         public async Task Update(int id, ItemModel model)
         {
             // Get the item
@@ -45,11 +60,13 @@ namespace Minecraft.Crafting.Services.DataItemsService
             await _http.PutAsJsonAsync($"https://localhost:7234/api/Crafting/{id}", item);
         }
 
+        /// <inheritdoc/>
         public async Task Delete(int id)
         {
             await _http.DeleteAsync($"https://localhost:7234/api/Crafting/{id}");
         }
 
+        /// <inheritdoc/>
         public async Task<List<CraftingRecipe>> GetRecipes()
         {
             return await _http.GetFromJsonAsync<List<CraftingRecipe>>("https://localhost:7234/api/Crafting/recipe");
